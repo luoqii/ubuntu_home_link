25a0fd659af49feabfc855f8760129363c62d01b
bb.S <bangbang.S@gmail.com>
2015-09-07 13:48:46 +0800

    add download status check.

--- workbench/bin/dh_helper.sh
+++ workbench/bin/dh_helper.sh
@@ -14,9 +14,15 @@ if [[ $line =~ bysongstart ]] ; then
 	bash -c "$cmd"
 fi
 if [[ $line =~ bysongend ]] ; then
-        cmd=`echo $line |sed -e 's/bysongend|\([^|]*\)|\([^|]*\)/rm \1.txt/'`
-        echo cmd: $cmd
-        bash -c "$cmd"
+        file=`echo $line |sed -e 's/bysongend|\([^|]*\)|\([^|]*\)/\1/'`
+	echo file: $file
+	head=`head -c 4 $file`
+	echo head: $head
+	if [[ "$head" != "<?xm" ]] ; then
+        	cmd=`echo $line |sed -e 's/bysongend|\([^|]*\)|\([^|]*\)/rm \1.txt/'`
+        	echo cmd: $cmd
+        	bash -c "$cmd"
+        fi
 fi
 
 done
